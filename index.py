464c7737b9466be99dbf12cc62858f17c9ba3085
Kasfil Aziz <kasf@tuta.io>
2021-02-13 15:51:34 +0700

    integrate v1 routes to index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from api.v1 import routes
 
 from app.configs import configs
 
@@ -8,19 +9,4 @@ app = FastAPI(
     debug = configs.DEBUG_MODE
 )
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Optional[bool] = None
-
-@app.get('/')
-def home():
-    return {'message': 'Hellow'}
-
-@app.get('/items/{item_id}')
-def item_id(item_id: int, q: Optional[str] = None):
-    return {'id': item_id, 'q': q}
-
-@app.post('/items/{item_id}')
-def add_item(item_id: int, item: Item):
-    return {'item_name': item.name, 'item_id': item_id}
+app.include_router(routes.routes, prefix='/api/v1')
